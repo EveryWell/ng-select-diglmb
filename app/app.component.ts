61fb66c3ec15794686074f3ef1d9551d66959d2a
EveryWell <aneverywell@gmail.com>
2019-03-20 08:22:37 +0100

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component, NgModule, ViewChild} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormControl, FormGroup, ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {NgSelectModule, NgOption} from '@ng-select/ng-select';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Angular ng-select <small class="text-muted"><a target="_blank" href="https://github.com/ng-select/ng-select">Open in Github</a></small></h1>
+        <ng-select
+            [items]="companies"
+            [multiple]="true"
+            bindLabel="name"
+            bindValue="id"
+            placeholder="All companies">
+            <ng-template ng-multi-label-tmp let-items="items" let-clear="clear">
+                <span class="ng-value">{{ items.length }} companies</span>
+            </ng-template>
+        </ng-select>
+`
+})
+export class AppComponent {
+
+    companies = [
+        {id: 1, name: 'Company 1'},
+        {id: 2, name: 'Company 2'},
+        {id: 3, name: 'Company 3'},
+        {id: 4, name: 'Company 4'},
+        {id: 5, name: 'Company 5'}
+    ];
+}
