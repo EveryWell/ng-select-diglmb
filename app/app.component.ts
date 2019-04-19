13073e61d8b6fb9c5d432aad5bf65a29bfaa468c
EveryWell <aneverywell@gmail.com>
2019-04-19 12:09:16 +0200

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,12 +16,51 @@ import {NgSelectModule, NgOption} from '@ng-select/ng-select';
             <ng-template ng-multi-label-tmp let-items="items" let-clear="clear">
                 <span class="ng-value">{{ items.length }} companies</span>
             </ng-template>
+            <ng-template ng-option-tmp let-item="item" let-item$="item$" let-index="index">
+              <label class="ng-checkbox-container">
+                <input id="item-{{index}}" type="checkbox" [ngModel]="item$.selected">
+                <span class="checkmark"></span>
+              </label>
+              <div class="ng-value-container">
+                <span class="ng-value">{{item.name}}</span>
+              </div>
+            </ng-template>
         </ng-select>
 `
 })
 export class AppComponent {
 
     companies = [
+        {id: 1, name: 'Company 1'},
+        {id: 2, name: 'Company 2'},
+        {id: 3, name: 'Company 3'},
+        {id: 4, name: 'Company 4'},
+        {id: 5, name: 'Company 5'},
+        {id: 1, name: 'Company 1'},
+        {id: 2, name: 'Company 2'},
+        {id: 3, name: 'Company 3'},
+        {id: 4, name: 'Company 4'},
+        {id: 5, name: 'Company 5'},
+        {id: 1, name: 'Company 1'},
+        {id: 2, name: 'Company 2'},
+        {id: 3, name: 'Company 3'},
+        {id: 4, name: 'Company 4'},
+        {id: 5, name: 'Company 5'},
+        {id: 1, name: 'Company 1'},
+        {id: 2, name: 'Company 2'},
+        {id: 3, name: 'Company 3'},
+        {id: 4, name: 'Company 4'},
+        {id: 5, name: 'Company 5'},
+        {id: 1, name: 'Company 1'},
+        {id: 2, name: 'Company 2'},
+        {id: 3, name: 'Company 3'},
+        {id: 4, name: 'Company 4'},
+        {id: 5, name: 'Company 5'},
+        {id: 1, name: 'Company 1'},
+        {id: 2, name: 'Company 2'},
+        {id: 3, name: 'Company 3'},
+        {id: 4, name: 'Company 4'},
+        {id: 5, name: 'Company 5'},
         {id: 1, name: 'Company 1'},
         {id: 2, name: 'Company 2'},
         {id: 3, name: 'Company 3'},
